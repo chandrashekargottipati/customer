cd40114ddbbf7bd90cfdca3336ffa42b8183732a
chandra shekar <chandrashekargottipati@gmil.com>
2024-07-01 12:18:19 +0530

    chore: change mysql to  postgreesql

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer(1, "John Doe", "john.doe@example.com");
-        Mysqlconnectdb repositry = new Mysqlconnectdb();
-        CustomerService customerService = new CustomerServiceImpl(repositry);
+//        Mysqlconnectdb repositry = new Mysqlconnectdb();
+        Postgresql postgresql = new Postgresql();
+//        CustomerService customerService = new CustomerServiceImpl(repositry);
+        CustomerService customerService = new CustomerServiceImpl(postgresql);
+
         customerService.addCustomertodb(customer);
         customerService.displaycustomerdata(customer);
+
     }
 }
