1e143eda4c89ef251c4d8d0ed938d088d0d85d4e
chandra shekar <chandrashekargottipati@gamil.com>
2024-07-21 01:24:23 +0530

    add address class

---
+++ src/main/java/com/helloworld/java/feature/Address.java
@@ -0,0 +1,14 @@
+package com.helloworld.java.feature;
+
+public class Address {
+    String city;
+
+    String state;
+    String country;
+
+    public Address(String city, String state, String country) {
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+}
